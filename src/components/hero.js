035d946e5f6a226e1499042140ff66221ae44013
Stephanie Green <stephanie.green@gsa.gov>
2020-08-17 09:58:38 -0600

    update image credit

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,7 +7,7 @@ import React from 'react';
 const Hero = () => (
   <section className="usa-hero height-mobile">
     <p className="hero-transparent">
-      Image by Rassi G. Borneo - <a href="https://www.timelinedc.com/index">www.timelinedc.com</a>
+Image: Rassi G. Borneo, <a href="https://www.timelinedc.com/index">Timeline Media</a>
     {/* <div className="grid-container">
       <div className="usa-hero__callout">
         <h2 className="usa-hero__heading">
